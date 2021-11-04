104bcba706d02d657bf672e12118af0d2b0f6628
Saulius Kazokas <saulens22@gmail.com>
2021-11-04 23:45:27 +0200

    First debian config

---
+++ debian-setup.sh
@@ -0,0 +1,19 @@
+sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+sudo chmod +x /usr/local/bin/oh-my-posh
+
+mkdir ~/.poshthemes
+wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
+unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
+chmod u+rw ~/.poshthemes/*.json
+rm ~/.poshthemes/themes.zip
+
+echo 'eval "$(oh-my-posh --init --shell bash --config ~/.poshthemes/jandedobbeleer.omp.json)"' >> ~/.bashrc
+
+echo "bind 'TAB:menu-complete'" >> ~/.bashrc
+echo 'bind "set show-all-if-ambiguous on"' >> ~/.bashrc
+echo 'bind "set menu-complete-display-prefix on"' >> ~/.bashrc
+
+
+
+
+source ~/.bashrc

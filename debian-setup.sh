8e74ace6c51e4d1cbc61863809456fa0c1e75745
Saulius Kazokas <9000854+saulens22@users.noreply.github.com>
2023-02-06 03:12:21 +0200

    btop update

--- debian-setup.sh
+++ debian-setup.sh
@@ -17,8 +17,8 @@ echo 'bind "set menu-complete-display-prefix on"' >> ~/.bashrc
 sudo cp -r ./nerdfonts /usr/share/fonts/truetype/nerdfonts
 
 mkdir ~/btop-install && cd ~/btop-install
-wget https://github.com/aristocratos/btop/releases/download/v1.1.1/btop-1.1.1-x86_64-linux-musl.tbz
-tar -xjf btop-1.1.1-x86_64-linux-musl.tbz
+wget https://github.com/aristocratos/btop/releases/download/v1.2.13/btop-x86_64-linux-musl.tbz
+tar -xjf btop-x86_64-linux-musl.tbz
 sudo make install
 
 echo "alias sail='[ -f sail ] && bash sail || bash vendor/bin/sail'" >> ~/.bashrc
